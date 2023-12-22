734aabc9430f66f4e4edeea6fa8f742bb9f7c8b2
Dakota Whitney <whitneydakota@gmail.com>
2023-12-22 14:23:56 -0500

    Use slider-template

--- js/components/about.js
+++ js/components/about.js
@@ -1,10 +1,16 @@
+import config from "./config.json" assert {type: 'json'};
+
 export class AboutPage extends HTMLElement {
     constructor(){
         super();
         AboutPage.shadowRoot = this.attachShadow({mode: "open"});
     }
-    connectedCallback(){
+    async connectedCallback(){
         console.log(this.constructor.name + " connected to DOM");
+
+        // const response = await fetch(`https://script.google.com/macros/s/${config.gId}/exec?page=about`);
+        // const likes = await response.json();
+        // console.log(likes);
     }
     disconnectedCallback() {
         console.log(this.constructor.name + " removed from DOM");
--- js/slider.js
+++ js/slider.js
@@ -5,18 +5,78 @@ import { CredentialsPage } from "./components/credentials.js"
 import { ContactPage } from "./components/contact.js"
 
 export class Slider {
-  static content = {
+  static custom = {
     about: AboutPage,
     projects: ProjectsPage,
     resume: ResumePage,
     credentials: CredentialsPage,
     contact: ContactPage
   }
-  static tag(customName) {
+  static customTag(customName) {
     return customName.replace(/(?<=[a-z])[A-Z]/, m => "-" + m).toLowerCase();
   }
+  static customTemplate(customName){
+    const templateId = customName.replace(/(?<=[a-z])[A-Z]\w+/, "").toLowerCase() + "-template";
+    const template = document.getElementById(templateId).content;
+    return(template);
+  }
   static {
-    const carousel = document.getElementById(this.name.toLowerCase());
-    this.carousel = bootstrap.Carousel.getOrCreateInstance(carousel);
+    const nav = document.querySelector("ul.navbar-nav");
+
+    for(const [page, custom] of Object.entries(this.custom)){
+      const tag = this.customTag(custom.name);
+      customElements.define(tag, custom)
+
+      const pageNav = document.createElement("li");
+      pageNav.classList.add("nav-item");
+      pageNav.innerHTML = `<a class="nav-link" aria-current="page">${page[0].toUpperCase() + page.substring(1)}</a>`;
+      nav.append(pageNav);
+
+      const customPage = document.createElement(tag);
+      const template = this.customTemplate(custom.name);
+
+      customPage.shadowRoot.append(template.cloneNode(true));
+      customPage.id = tag;
+      customPage.classList.add("carousel-item");
+
+      this.custom[page] = customPage;
+    };
+
+    this.nav = [...nav.children];
+    this.prefix = this.name.toLowerCase();
+    this.template = document.getElementById(`${this.prefix}-template`).content;
+  }
+  constructor(root = "main", id = Slider.prefix, content = Slider.custom){
+    root = document.querySelector(root);
+    root.append(Slider.template.cloneNode(true));
+    [ root ] = root.children;
+    root.id = id;
+
+    const controls = root.querySelectorAll("button[class*='carousel-control']");
+    for(const control of controls) control.setAttribute("data-bs-target", "#" + id);
+
+    const styleSheets = document.querySelectorAll("link[rel='stylesheet']");
+    const innerRoot = root.querySelector(".carousel-inner");
+
+    for(const [contentId, contentEl] of Object.entries(content)){
+      if(id === Slider.prefix){
+        for(const styleSheet of styleSheets){
+          contentEl.shadowRoot.prepend(styleSheet.cloneNode(false));
+        };
+      }
+      else contentEl.id = contentId;
+      content[contentId] = innerRoot.appendChild(contentEl);
+    };
+
+    this.carousel = bootstrap.Carousel.getOrCreateInstance(root);
+
+    if(id === Slider.prefix){
+      Slider.carousel = this.carousel;
+      Slider.nav.forEach((pageNav, i) => {
+        pageNav.addEventListener("click", () => Slider.carousel.to(i));
+      });
+    };
+
+    innerRoot.children[0].classList.add("active");
   }
 }
