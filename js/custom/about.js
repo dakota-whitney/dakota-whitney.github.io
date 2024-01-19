a9e0ff04979b82a819cb51075b825b13038f70f3
Dakota Whitney <whitneydakota@gmail.com>
2024-01-19 15:12:09 -0500

    Add Apps Script comment

--- js/custom/about.js
+++ js/custom/about.js
@@ -1,6 +1,7 @@
 import { CustomTemplate, Card, customTag, customPrefix } from "./custom.js";
-import { gId } from "./config.js";
+import { gId } from "./config.js"; //Import Google Apps Script public endpoint ID from local config.js file
 
+//Query my public Apps Script endpoint!
 async function gFetch(pageId, query = {}){
   const gURL = new URL(`macros/s/${gId}/exec`, "https://script.google.com");
   gURL.search = new URLSearchParams({page: pageId, ...query});
