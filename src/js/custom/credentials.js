fe139f5ecf0a8526eaa09b3de3883a60ebb3251f
Dakota Whitney <whitneydakota@gmail.com>
2025-08-20 21:30:39 -0400

    Revert "Remove config.js"
    This reverts commit 78f692de72fd2d02840d398009b4d6984a9e3593.

--- src/js/custom/credentials.js
+++ src/js/custom/credentials.js
@@ -2,15 +2,15 @@ import { CustomTemplate } from "./custom.js";
 import { cId } from "../../../src/config.js";
 
 export class CredentialsPage extends CustomTemplate {
-    static base = new URL();
+    static base = new URL("https://drive.google.com");
     constructor(){
         super();
         const {base} = CredentialsPage;
-        // this.cURL = new URL(`file/d/${cId}/preview`, base.href);
+        this.cURL = new URL(`file/d/${cId}/preview`, base.href);
     }
     connectedCallback(){
         console.log(this.constructor.name + " connected to DOM");
         this.cloneTemplate(this.constructor.name);
-        // this.querySelector("#fs-web").src = this.cURL;
+        this.querySelector("#fs-web").src = this.cURL;
     }
 }
