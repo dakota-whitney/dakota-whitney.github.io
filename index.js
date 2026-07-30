cbcefb365a3e01abe36c7dd4aecb2cf04a5ce0db
Dakota Whitney <55603071+dakota-whitney@users.noreply.github.com>
2026-07-30 07:28:50 -0400

    Remove extra headings and update styles

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-import { getRepos } from "./repos.js";
-
+import { RepoLink } from "./repos.js";
 document.querySelector(".brand-text").innerText = document.title;
-
-window.spinner = document.querySelector(".spinner-border");
-window.repoAlert = document.querySelector(".alert");
-
-const repoLinks = document.querySelector(".sidebar-menu");
-
-getRepos()
-    .then(repos => {
-        spinner.classList.add("d-none");
-        repos.forEach((repo, i) => {
-            console.log(repo);
-            const repoLink = document.createElement("li", {is: "repo-link"});
-            repoLink.dataset.repoName = repo.name;
-            repoLinks.append(repoLink);
-            if(!i) repoLink.displayReadMe();
-        });
-    })
-    .catch(error => {
-        spinner.classList.add("d-none");
-        repoAlert.classList.remove("d-none");
-        console.error(error);
-    });
+RepoLink.getRepos().then(() => window.addEventListener('popstate', e => RepoLink.onNavigate(e)))
--- repos.js
+++ repos.js
@@ -24,73 +24,87 @@ const readMeQuery = {
     }
 };
 
-export async function getRepos(){
-    const { data: repos } = await octokit.rest.repos.listForUser(repoQuery);
-    return repos;
-};
-
-// const spinner = document.querySelector(".spinner-border");
-// const alert = document.querySelector(".alert");
+const sidebar = document.querySelector(".sidebar-menu");
+const spinner = document.querySelector(".spinner-border");
+const alert = document.querySelector(".alert");
 const repo = document.getElementById("repo");
 
 // Instantiate new DOM parser for the read me file
 const parser = new DOMParser();
 
-class RepoLink extends HTMLLIElement {
+export class RepoLink extends HTMLElement {
+    static async getRepos(){
+        this.loading(true);
+        try {
+            const { data: repos } = await octokit.rest.repos.listForUser(repoQuery);
+            repos.forEach(this.append);
+            history.replaceState({repo: repos[0].name}, "");
+        } catch(error) {
+            alert.classList.remove("d-none");
+            console.error(error);
+        };
+        this.loading(false);
+    };
+    static append(repo, i){
+        const repoLink = document.createElement("repo-link");
+        repoLink.dataset.repoName = repo.name;
+        sidebar.append(repoLink);
+        if(!i) repoLink.render();
+    };
+    static loading(show = true){
+        if(show) spinner.classList.remove("d-none");
+        else spinner.classList.add("d-none");
+    };
+    static onNavigate(event){
+        if(!event.state) return;
+        const repo = event.state.repo;
+        const selector = `repo-link[data-repo-name="${repo}"]`;
+        sidebar.querySelector(selector).render();
+    };
     constructor(){
         super();
-        this.onclick = () => this.displayReadMe();
+        this.onclick = () => this.render();
     };
-
     connectedCallback(){
-        console.log(this.dataset.repoName + " connected to DOM");
-        this.classList.add("nav-item");
-        const navAnchor = document.createElement("a");
-        navAnchor.classList.add("nav-link");
-        navAnchor.href = "#" + this.dataset.repoName;
-        navAnchor.innerHTML = "<i class='nav-icon bi bi-git'></i>";
-        navAnchor.innerHTML += `<p>${this.dataset.repoName}</p>`
-        this.append(navAnchor);
+        const a = document.createElement("a");
+        a.classList.add("nav-link");
+        a.href = "#" + this.dataset.repoName;
+        a.innerHTML = "<i class='nav-icon bi bi-git'></i>";
+        a.innerHTML += `<p>${this.dataset.repoName}</p>`;
+        const li = document.createElement("li");
+        li.classList.add("nav-item");
+        li.append(a);
+        this.append(li);
     };
-
-    displayReadMe(){
+    async render(){
         document.querySelector(".nav-item.active")?.classList.remove("active");
-        this.classList.add("active");
-
+        this.querySelector("a").classList.add("active");
         if(this.readMe) return repo.innerHTML = this.readMe;
-
         repo.innerHTML = "";
-        spinner.classList.remove("d-none");
 
-        this.getReadMe()
-            .then(() => {
-                spinner.classList.add("d-none");
-                repo.innerHTML += this.readMe;
-            })
-            .catch(error => {
-                spinner.classList.add("d-none");
-                repoAlert.classList.remove("d-none");
-                console.error(error);
-            });
+        RepoLink.loading(true);
+        try {
+            await this.getReadMe();
+            repo.innerHTML += this.readMe;
+            history.pushState({repo: this.dataset.repoName}, "")
+        } catch(e) {
+            alert.classList.remove("d-none");
+            console.error(error);
+        };
+        RepoLink.loading(false);
     };
-
     async getReadMe(){
         const rmQuery = {...readMeQuery, repo: this.dataset.repoName}
         const { data } = await octokit.rest.repos.getReadme(rmQuery);
         const readMe = DOMPurify.sanitize(data);
         this._readMe = parser.parseFromString(readMe, "text/html");
-        const header = document.createElement("h1");
-        header.classList.add("text-decoration-underline");
-        header.innerText = this.dataset.repoName;
         const readMeRoot = this._readMe.getElementById("readme");
-        readMeRoot.insertBefore(header, readMeRoot.children[0]);
         this._readMe.querySelector("article").classList.replace("container-lg", "container-fluid");
         this._readMe.querySelectorAll(".anchor").forEach(anchor => anchor.remove());
     };
-
     get readMe(){
         return this._readMe?.getElementById("readme").outerHTML;
     };
 };
 
-customElements.define("repo-link", RepoLink, {extends: "li"});
+customElements.define("repo-link", RepoLink);
