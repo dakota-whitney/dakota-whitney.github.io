802a292efd5716362ecd0ac48244520dd895e413
Dakota Whitney <55603071+dakota-whitney@users.noreply.github.com>
2026-09-08 08:36:13 -0400

    Use Bootstrap template

--- repos.js
+++ repos.js
@@ -25,7 +25,7 @@ const readMeQuery = {
 };
 
 // Set references to DOM elements
-const sidebar = document.querySelector(".sidebar-menu");
+const sidebar = document.querySelector(".sidebar ul.nav");
 const spinner = document.querySelector(".spinner-border");
 const alert = document.querySelector(".alert");
 const repo = document.getElementById("repo");
@@ -35,28 +35,32 @@ const parser = new DOMParser();
 
 // Define new custom element for repository links
 export class RepoLink extends HTMLElement {
-    #readMe;
+  #readMe;
 
   // Define static method for fetching all repositories
+  // Append them to the sidebar
   static async getRepos() {
     this.loading(true);
+
     try {
       const { data: repos } = await octokit.rest.repos.listForUser(repoQuery);
-      repos.forEach(this.append);
+      const repoLinks = repos.map((repo) => this.create(repo));
+      repoLinks.forEach((repoLink) => sidebar.append(repoLink));
+      repoLinks[0].render(false);
       history.replaceState({ repo: repos[0].name }, "", `#${repos[0].name}`);
     } catch (error) {
       alert.classList.remove("d-none");
       console.error(error);
     }
+
     this.loading(false);
   }
 
-  // Define static method for appending repository links
-  static async append(repo, i) {
+  // Define static method for creating repository links
+  static create(repo) {
     const repoLink = document.createElement("repo-link");
     repoLink.dataset.repoName = repo.name;
-    sidebar.append(repoLink);
-    if (!i) await repoLink.render(false);
+    return repoLink;
   }
 
   // Define static method for showing/hiding loading spinner
@@ -84,35 +88,45 @@ export class RepoLink extends HTMLElement {
   // Create AdminLTE sidebar elements using DOM APIs
   connectedCallback() {
     const a = document.createElement("a");
-    a.classList.add("nav-link");
-    a.innerHTML = "<i class='nav-icon bi bi-git'></i>";
-    a.innerHTML += `<p>${this.dataset.repoName}</p>`;
+    a.classList.add(
+      "nav-link",
+      "d-flex",
+      "align-items-center",
+      "gap-2",
+      "active",
+    );
+    a.ariaCurrent = "page";
+    a.innerHTML = `
+      <svg class="bi" aria-hidden="true">
+        <use xlink:href="#git"></use>
+      </svg>
+    `;
+    a.innerHTML += this.dataset.repoName;
     const li = document.createElement("li");
     li.classList.add("nav-item");
+    li.style.cursor = "pointer";
     li.append(a);
     this.append(li);
   }
 
   // Define render method for the dispaying the README
+  // Add repo to history if a different one was clicked
   async render(addHistory = true) {
     document.querySelector(".nav-item.active")?.classList.remove("active");
     this.querySelector("a").classList.add("active");
 
-    if (addHistory)
+    if (addHistory && this.dataset.repoName !== history.state.repo)
       history.pushState(
         { repo: this.dataset.repoName },
         "",
         `#${this.dataset.repoName}`,
       );
 
-    if (this.readMe) return (repo.innerHTML = this.readMe);
-    repo.innerHTML = "";
-
     RepoLink.loading(true);
 
     try {
-      await this.getReadMe();
-      repo.innerHTML += this.readMe;
+      if (!this.readMe) await this.getReadMe();
+      repo.innerHTML = this.readMe;
     } catch (error) {
       alert.classList.remove("d-none");
       console.error(error);
@@ -139,7 +153,6 @@ export class RepoLink extends HTMLElement {
   get readMe() {
     return this.#readMe?.getElementById("readme").outerHTML;
   }
-
 }
 
 // Define the repo-link custom element
